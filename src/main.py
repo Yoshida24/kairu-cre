6bae98ad33b7b2649b806eaa53b4700ac6017d89
Yoshida24 <trihead1.dev@gmail.com>
2024-03-21 01:16:53 +0900

    feat: main関数を実装

--- src/main.py
+++ src/main.py
@@ -1,12 +1,36 @@
-from modules.cowsay_demo.greeting import greet_to, greet_from_env
+from modules.message_input.cli import message_input
+from thought.clarify_issue.clarify_issue import clarify_issue
+from thought.summarize_issue.summarize_issue import summarize_issue
+from thought.retrieve_similar_issue.retrieve_similar_issue import retrieve_similar_issue
+
+
+"""
+Issueの一例（Google製品のサポートページより）:
+
+URLを掲載していないページからの流入が計測されてしまいます。
+あるWEBページにどのメディアから来ているか調べたく、
+レポート＞エンゲージメント＞ランディングページ内で、
+トラフィックソースを「セッションの参照元／メディア」で検索しました。
+
+しかし数は少ないですが、バナーを掲載していないページからの流入がいくつか見られました。
+こちら原因などご存知の方いらっしゃいますでしょうか？
+"""
 
 
 def main():
-    # use argument to greet
-    greet_to(your_name="Alan")
+    # ユーザーにIssueを入力してもらう
+    issue_content = message_input()
 
-    # use environment variable to greet
-    greet_from_env()
+    # 処理中のログ
+    print(f"\nあなたの質問: {issue_content}\n")
+    print("\n回答を生成中...\n")
+
+    # Issueのサマリを作成
+    summarize_issue(issue_content=issue_content)
+
+    # Issueの不足情報をリストアップして確認する
+    clarify_issue(issue_content=issue_content)
+
 
 
 if __name__ == "__main__":
